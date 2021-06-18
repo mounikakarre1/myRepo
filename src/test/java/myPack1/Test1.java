372f0bd32247a6e0073f9f37529d5d23256a3d35
mounikakarre1 <85626830+mounikakarre1@users.noreply.github.com>
2021-06-18 13:18:41 +0530

    Update Test1.java

--- src/test/java/myPack1/Test1.java
+++ src/test/java/myPack1/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Hi,did change5 after integrating with Jenkins....");
+		System.out.println("Hi,did change6 after integrating with Jenkins....");
 		System.out.println("added extra");
 	}
 
