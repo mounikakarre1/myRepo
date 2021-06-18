2873a3f888bfc081922f0b4f6d780d9ca0527772
mounika.karre <mounika.karre@NODE1482.gspann.local>
2021-06-18 20:25:53 +0530

    New changes in eclipse

--- src/test/java/myPack1/Test1.java
+++ src/test/java/myPack1/Test1.java
@@ -5,7 +5,7 @@ public class Test1 {
 	public static void main(String[] args)
 	{
 		System.out.println("Hi,did change6 after integrating with Jenkins....");
-		System.out.println("added extra");
+		System.out.println("New changes in eclipse");
 	}
 
 }
